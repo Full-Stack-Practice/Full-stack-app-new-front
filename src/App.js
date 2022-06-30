48212e64c10836ebc615b56556ef019983ed3e24
Anddy <alonzoa2015@gmail.com>
2022-06-30 15:47:03 -0700

    stuff

--- src/App.js
+++ src/App.js
@@ -1,48 +1,48 @@
+import { useState } from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import './App.css';
 import AuthPage from './AuthPage';
 
 function App() {
   const [user, setUser] = useState({});
   
+  console.log(user, 'user');
 
-  useEffect(() => {
-    const user = cookies.get('session');
-    if (user) {
-      setUser(user);
-    }
-  })
   return (
     <Router>
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/albums">Albums</Link>
-          </li>
-          <li>
-            <Link to="/users">Users</Link>
-          </li>
-        </ul>
-      </nav>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/albums">Albums</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
 
-      {/* A <Switch> looks through its children <Route>s and
+        {/* A <Switch> looks through its children <Route>s and
           renders the first one that matches the current URL. */}
-      <Switch>
-        <Route path="/albums">
-          {/* <About /> */}
-        </Route>
-        <Route path="/users">
-          {/* <Users /> */}
-        </Route>
-        <Route path="/">
-          <AuthPage />
-        </Route>
-      </Switch>
-    </div>
-  </Router>
+        <Switch>
+          <Route path="/albums">
+            {/* <About /> */}
+            <div>
+              {user.username}
+            </div>
+          </Route>
+          <Route path="/users">
+            {/* <Users /> */}
+          </Route>
+          <Route path="/">
+            <AuthPage setUser= {setUser}/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/AuthPage.js
+++ src/AuthPage.js
@@ -1,16 +1,15 @@
 import React, { useState } from 'react';
-import { signUpUser, signInUser } from './services/fetch-utils';
 import { useHistory } from 'react-router-dom';
+import { signUpUser, signInUser, getUser } from './services/fetch-utils';
 
-export default function AuthPage() {
+export default function AuthPage({ setUser }) {
   const { push } = useHistory();
 
-const [signUpEmail, setSignUpEmail] = useState('');
+  const [signUpEmail, setSignUpEmail] = useState('');
   const [signUpPassword, setSignUpPassword] = useState('');  
   const [signInEmail, setSignInEmail] = useState('');
   const [signUpUsername, setSignUpUsername] = useState('');
   const [signInPassword, setSignInPassword] = useState('');
-  const [currentUser, setCurrentUser] = useState({});
 
   function clearForms() {
     setSignInEmail('');
@@ -23,8 +22,9 @@ const [signUpEmail, setSignUpEmail] = useState('');
   async function handleSignUp(e) {
     e.preventDefault();
 
-    const user = await signUpUser(signUpUsername, signUpEmail, signUpPassword);
-    setCurrentUser(user);
+    await signUpUser(signUpUsername, signUpEmail, signUpPassword);
+    const user = await getUser();
+    setUser(user);
     push('/albums');
     clearForms();
   }
@@ -32,47 +32,48 @@ const [signUpEmail, setSignUpEmail] = useState('');
   async function handleSignIn(e) {
     e.preventDefault();
 
-    const user = await signInUser(signInEmail, signInPassword);
-    setCurrentUser(user);
+    await signInUser(signInEmail, signInPassword);
+    const user = await getUser();
+    setUser(user);
     push('/albums');
     clearForms();
   }
 
-    return (
-        <div className="home-page">
-          <div className='login-form'>
-            <form className="form-input" onSubmit={handleSignUp}>
-              <h2>Sign Up</h2>
-              <label>
-                <p>Username</p>
-                <input value={signUpUsername} onChange={e => setSignUpUsername(e.target.value)} />
-              </label>
-              <label>
-                <p>Email</p>
-                <input value={signUpEmail} onChange={e => setSignUpEmail(e.target.value)} />
-              </label>
-              <label>
-                <p>Password</p>
-                <input value={signUpPassword} type='password' onChange={e => setSignUpPassword(e.target.value)} />
-              </label>
-              <button>Sign Up</button>
-            </form>
-          </div>
-          <div className='login-form'>
-            <form className="form-input" onSubmit={handleSignIn}>
-              <h2>Sign In</h2>
-              <label>
-                <p>Email</p>
-                <input value={signInEmail} onChange={e => setSignInEmail(e.target.value)} />
-              </label>
-              <label>
-                <p>Password</p>
-                <input value={signInPassword} type='password' onChange={e => setSignInPassword(e.target.value)} />
-              </label>
-              <button>Sign In</button>
-            </form>
+  return (
+    <div className="home-page">
+      <div className='login-form'>
+        <form className="form-input" onSubmit={handleSignUp}>
+          <h2>Sign Up</h2>
+          <label>
+            <p>Username</p>
+            <input value={signUpUsername} onChange={e => setSignUpUsername(e.target.value)} />
+          </label>
+          <label>
+            <p>Email</p>
+            <input value={signUpEmail} onChange={e => setSignUpEmail(e.target.value)} />
+          </label>
+          <label>
+            <p>Password</p>
+            <input value={signUpPassword} type='password' onChange={e => setSignUpPassword(e.target.value)} />
+          </label>
+          <button>Sign Up</button>
+        </form>
+      </div>
+      <div className='login-form'>
+        <form className="form-input" onSubmit={handleSignIn}>
+          <h2>Sign In</h2>
+          <label>
+            <p>Email</p>
+            <input value={signInEmail} onChange={e => setSignInEmail(e.target.value)} />
+          </label>
+          <label>
+            <p>Password</p>
+            <input value={signInPassword} type='password' onChange={e => setSignInPassword(e.target.value)} />
+          </label>
+          <button>Sign In</button>
+        </form>
     
-          </div>
-        </div>
-      );
+      </div>
+    </div>
+  );
 }
